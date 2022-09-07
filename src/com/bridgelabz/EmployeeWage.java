7f8c4fcff320d6325aec1233e0064dfb294ff8bd
Pranali-22 <lpranali22@gmail.com>
2022-09-07 09:28:28 +0530

    Added: UC3: Part time employee

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,6 +3,8 @@
  */
 package com.bridgelabz;
 
+import java.util.*;
+
 /**
  * @author Dell
  *
@@ -12,21 +14,29 @@ public class EmployeeWage {
 	/**
 	 * @param args
 	 */
-	public void employeeIsPresentOrNot() {
-		double a = Math.random();
+	
+	public static void main(String[] args) {
+		// Driving code
+		Random r = new Random();
+		int a=r.nextInt();
+		final int fullDayHr=8, partDayHr=4, wagePerHr=20;;
+		int salary; 
 		
-		if(a>=0.5) {
+		if(a%3==0) {
 			System.out.println("Employee is present");
+			salary=wagePerHr * fullDayHr;
+			System.out.println("Daily Wage = "+salary);
+		}
+		else if(a%3==1) {
+			System.out.println("Employee is present parttime");
+			salary=wagePerHr * partDayHr;
+			System.out.println("Daily Wage = "+salary);
 		}
 		else{
 			System.out.println("Employee is absent");
+			System.out.println("Daily Wage = "+0);
 		}
-	}
-	public static void main(String[] args) {
-		// Driving code
-		EmployeeWage e1 = new EmployeeWage();
-		e1.employeeIsPresentOrNot();
 		
-		}
+	}
 
 }
