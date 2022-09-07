f629116ec52e0585d4fb6e0c024b75f414fdf537
Pranali-22 <lpranali22@gmail.com>
2022-09-07 18:06:11 +0530

    Added: UC4: Part time employee using switch case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -21,20 +21,26 @@ public class EmployeeWage {
 		int a=r.nextInt();
 		final int fullDayHr=8, partDayHr=4, wagePerHr=20;;
 		int salary; 
-		
-		if(a%3==0) {
-			System.out.println("Employee is present");
-			salary=wagePerHr * fullDayHr;
-			System.out.println("Daily Wage = "+salary);
-		}
-		else if(a%3==1) {
-			System.out.println("Employee is present parttime");
-			salary=wagePerHr * partDayHr;
-			System.out.println("Daily Wage = "+salary);
-		}
-		else{
-			System.out.println("Employee is absent");
-			System.out.println("Daily Wage = "+0);
+				
+		switch (a%3) {
+			case 0:
+				System.out.println("Employee is present");
+				salary=wagePerHr * fullDayHr;
+				System.out.println("Daily Wage = "+salary);
+				break;
+				
+			case 1:
+				System.out.println("Employee is present parttime");
+				salary=wagePerHr * partDayHr;
+				System.out.println("Daily Wage = "+salary);
+				break;
+				
+			case 2:
+				System.out.println("Employee is absent");
+				System.out.println("Daily Wage = "+0);
+				break;
+			
+			
 		}
 		
 	}
